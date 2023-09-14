ad260e61a7fe2b1c55de3a45fef10d396c27bd78
Indhumathi <indhumathi.b2020cse@sece.ac.in>
2023-09-14 21:00:50 +0530

    initial commit

---
+++ url.js
@@ -0,0 +1,3 @@
+// module.exports = `mongodb://localhost:27017`
+module.exports=`mongodb+srv://indhumathib2020cse:Indhu%4006@cluster0.mdoshzm.mongodb.net/miniprj`
+// module.exports = `mongodb+srv://admin:admin@mdb.vtkja.mongodb.net`
