163211814559bf6b0ab5dd0592fa6ced128890b9
Indhumathi_B <88466628+IndhumathiBala@users.noreply.github.com>
2023-09-20 12:07:30 +0530

    Update fetch.js restored

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -40,12 +40,12 @@ router.post("/auth", (req, res) => {
             console.log('Error in connection:- ', err)
         else {
             let db = conn.db("miniprj")
-            db.collection('users').find().toArray((err, array) => {
+            db.collection('users').find(obj).toArray((err, array) => {
                 if (err)
                     console.log(err)
                 else {
                     if (array.length > 0)
-                        res.json({ 'auth': 'success', 'user': uname, 'array':array })
+                        res.json({ 'auth': 'success', 'user': uname })
                     else
                         res.json({ 'auth': 'failed' })
                     console.log('Auth response sent')
